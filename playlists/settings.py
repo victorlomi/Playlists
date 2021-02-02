e62ca592091df70fec270e61199a653e5881b593
victorlomi <victorlominyo@gmail.com>
2021-02-02 12:38:14 +0300

    add lists_app to installed apps and add index url and view to project

--- playlists/settings.py
+++ playlists/settings.py
@@ -34,6 +34,7 @@ LOGOUT_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
+    'lists_app.apps.ListsAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
