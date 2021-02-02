ceb406cc53c039d55198754dca0edb5d50c5e99d
victorlomi <victorlominyo@gmail.com>
2021-02-02 13:11:38 +0300

    create a profile model and create a signup form for it

--- lists_app/models.py
+++ lists_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = models.ImageField(upload_to="images/")
+    email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.user.username
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
--- lists_app/views.py
+++ lists_app/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from .models import Profile
+from .forms import SignUpForm
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("welcome to the lists app")
 
 def signup(request):
-    pass
+    if request.user.is_authenticated:
+        return redirect('homepage')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            print("handling signup")
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.email = form.cleaned_data.get('email')
+            user.profile.profile_photo = form.cleaned_data.get('profile_photo')
+            user.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            print("signed up")
+            return redirect('homepage')
+        else:
+            return render(request, 'registration/signup.html', {'form': form})
+    else:
+        form = SignUpForm()
+        return render(request, 'registration/signup.html', {'form': form})
